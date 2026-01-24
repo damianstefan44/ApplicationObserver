88aec1c8169635f5c2d46b3070f027e3cd668774
damianstefan44 <rivus44@gmail.com>
2026-01-24 18:07:33 +0100

    Added simple run with test parameters to main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from orchestrator.orchestrator import Orchestrator
 
-if __name__ == '__main__':
-    exit(0)
+def main():
+    orchestrator = Orchestrator(
+        interval=2,
+        window_size=10,
+        memory_threshold=100.0,
+        cpu_threshold=90.0,
+        storage_path="snapshots.jsonl"
+    )
+    orchestrator.run()
+
+if __name__ == "__main__":
+    main()
